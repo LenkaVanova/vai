63598951c80015076e51bab797cd01d284c1fdfc
lenkavanova <lenusiq30@gmail.com>
2020-10-22 13:31:56 +0200

    stranka

---
+++ hdhd.php
@@ -0,0 +1,13 @@
+<?php
+function kuk($param){
+    echo $param;
+    return $param;
+}
+
+$array = ["jdj","kdkdks","jdjdjs","dhshf","djfdfj"]
+    ?>
+var_dump($array);
+
+<h1>Ahoj, <b></b>!</h1>
+
+?>
